8c7ff0efdef19a938f17a5c4b2ad370f344ce0ac
Alberto <aldo10012@gmail.com>
2021-01-29 11:26:36 -0800

    fixed attributes & created withdraw() method

--- BankAccount.py
+++ BankAccount.py
@@ -4,7 +4,7 @@ import random
 
 class BankAccount():
     # attributes
-    def __init__(self, name, routing_number, balance):
+    def __init__(self, name):
         # to assign account_number with a random 8-digit number
         def random_8_digit_num(n):
             range_start = 10**(n-1)
@@ -13,8 +13,8 @@ class BankAccount():
         
         self.full_name = name
         self.account_number = random_8_digit_num(8)
-        self.routing_number = routing_number
-        self.balance = balance
+        self.routing_number = 987654321
+        self.balance = 0
 
         
     
@@ -26,11 +26,18 @@ class BankAccount():
         print(f'Amount deposited: ${amount}')
 
     # withdraw
+    def withdraw(self, amount):
+        self.balance -= amount
+        print(f'Amount withdrawed: ${amount}')
+        if amount > self.balance:
+            print("Insufficient funds.")
+            # and charge them with an overdraft fee of $10?
+
     # get_balance
     # add_interest
     # print_reciet
 
-my_account = BankAccount( "Alberto", 12, 100  )
+my_account = BankAccount( "Alberto")
 
 my_account.deposite(666)
 print(my_account.balance)
