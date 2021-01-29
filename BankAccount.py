4b81cb9b1e42a43b0abb3a8665cf194763912c01
Alberto <aldo10012@gmail.com>
2021-01-29 10:52:05 -0800

    created class with attributes

---
+++ BankAccount.py
@@ -0,0 +1,9 @@
+class BankAccount():
+    # attributes
+    def __init__(self, name, account_number, routing_number, balance):
+        self.full_name = name
+        self.account_number = account_number
+        self.routing_number = routing_number
+        self.balance = balance
+        
+    
